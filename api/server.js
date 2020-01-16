54d1f6a956ffb78e2ef21222f4b2db83bf7c2710
Lukas Jaronis <jaronisluke13@gmail.com>
2020-01-16 13:12:51 -0600

    updated package.json and server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const helmet = require('helmet');
 const postsRouter = require("../Post/post-router");
 const server = express();
+server.use(helmet());
 server.use(express.json());
 server.use('/api/posts', postsRouter);
 module.exports = server;
